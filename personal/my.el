9aa2090c4713bf13cb43a6a3f9027dab6290d009
djjolicoeur <djjolicoeur@gmail.com>
2014-04-09 13:22:14 -0400

    added octave stuff

--- personal/my.el
+++ personal/my.el
@@ -1,7 +1,11 @@
 ;;;code
 ;;; (disable-theme 'zenburn)
 (setq prelude-guru nil)
+(setq tab-width 2)
 
+(autoload 'octave-mode "octave-mod" nil t)
+(setq auto-mode-alist
+            (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 
 (global-set-key (kbd "RET") 'newline-and-indent)
