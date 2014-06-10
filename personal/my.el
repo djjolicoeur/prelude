b6f967d5161880e2bb3e03625d5908478d2a6551
Daniel Jolicoeur <djjolicoeur@gmail.com>
2014-06-09 20:06:05 -0400

    modified

--- personal/my.el
+++ personal/my.el
@@ -3,11 +3,17 @@
 (setq prelude-guru nil)
 (setq tab-width 2)
 
+;; rainbow delims
+(require 'rainbow-delimiters)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
+
+;; Octave stuff
 (autoload 'octave-mode "octave-mod" nil t)
 (setq auto-mode-alist
             (cons '("\\.m$" . octave-mode) auto-mode-alist))
 
 
+;; Newline + indent
 (global-set-key (kbd "RET") 'newline-and-indent)
 
 
